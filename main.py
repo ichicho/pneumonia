0307709729b07565f1b227daf0989b144a1d9f62
Yichi Zhang <ichicho@keio.jp>
2018-10-16 01:36:41 +0900

    edit main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 
 from DicomDataLoader import DicomDataLoader
 from LabelList import LabelList
+from IoU import IoU
 
 path_dicoms = './Data/train_dicoms'
 dataloader = DicomDataLoader(path_dicoms, batch_size=2)
@@ -12,10 +13,6 @@ labellist = LabelList(path_dicoms, path_labels)
 for sample in dataloader:
     img = sample['img']
     pids = sample['pid']
-    print(labellist[pids[0]])
-    plt.imshow(img[0])
-    plt.show()
-    print(labellist[pids[1]])
-    plt.imshow(img[1])
-    plt.show()
-    print()
+    for pid in pids:
+        for bbox in labellist[pid]['bboxs']:
+            print(bbox)
